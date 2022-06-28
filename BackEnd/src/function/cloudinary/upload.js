bb5f823cb125dfcfa72cd2e1406688818b169710
belljun3395 <195850@jnu.ac.kr>
2022-06-28 16:40:51 +0900

    [API] cloudinary try-catch 문으로 변경

--- BackEnd/src/function/cloudinary/upload.js
+++ BackEnd/src/function/cloudinary/upload.js
@@ -1,8 +1,14 @@
 const cloudinary = require('cloudinary').v2
 
 const upload = async function (req){
-    var result = await cloudinary.uploader.upload(req.file.path, {upload_preset: "hairlog"})
-    return result.url
+    
+    try{
+        var result = await cloudinary.uploader.upload(req.file.path, {upload_preset: "hairlog"})
+        return result.url
+    }catch(e){ 
+        console.log(e)
+    }
+
 }
 
 
