79e627377c21f0a43718a6e0854a56adb3a08bd9
belljun3395 <195850@jnu.ac.kr>
2022-07-08 09:20:13 +0900

    .

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ const redisClient = redis.createClient({url: process.env.REDIS_URL});
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-await redisClient.connect();
+redisClient.connect();
 
 const sessionOption = {
   resave: false,
