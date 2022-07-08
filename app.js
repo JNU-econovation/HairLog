b2787b783cef71b60a53b6c07d0d7c73df1c2afc
belljun3395 <195850@jnu.ac.kr>
2022-07-08 09:22:37 +0900

    .

--- app.js
+++ app.js
@@ -79,8 +79,6 @@ app.use(cookieParser());
 
 const redisClient = redis.createClient({url: process.env.REDIS_URL});
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-
 redisClient.connect();
 
 const sessionOption = {
