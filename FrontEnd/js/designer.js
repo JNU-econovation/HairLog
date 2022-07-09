29adc2fc6b68c3f998044adc570e75917d9a7422
nhy0052 <nhy0052@naver.com>
2022-07-09 21:44:41 +0900

    디자이너 등록 기능

--- FrontEnd/js/designer.js
+++ FrontEnd/js/designer.js
@@ -6,7 +6,38 @@ function showPopup() {
 }
 
 // 나중에 팝업 취소하고 닫기, 저장하고 닫기 두개로 나누기
-function closePopup() {
-	const popup = document.querySelector('#popup');
-  popup.classList.add('hide');
+function closePopup(isSave) {
+  if(isSave){    // 저장 하고 닫기
+    const popup = document.querySelector('#popup');
+    popup.classList.add('hide');
+    console.log("저장O");
+    
+    const designerName = document.querySelector("#inputDesignerName").value;
+    const designerSalon = document.querySelector("#inputDesignerPlace").value;
+    const fav = false;
+    const designerData = { designerName, designerSalon, fav };          // 전송할 객체
+    console.log(designerData);
+
+    fetch('http://localhost:3000/api/designer', {        // 서버로 보내고 결과 출력
+      headers: {
+        'Content-Type': 'application/json'       
+      },
+      method: 'POST',
+      body: JSON.stringify(designerData), 
+    }) 
+    .then((response) => response.text())
+    .then((result) => { 
+      Datas = JSON.parse(result);
+      // console.log(result);
+      console.log(Datas); 
+  
+    });
+
+  }
+  else{       // 저장 안하고 닫기
+    const popup = document.querySelector('#popup');
+    popup.classList.add('hide');
+    console.log("저장X")
+  }
+
 }
