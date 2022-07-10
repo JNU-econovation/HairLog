c00c2399e022d06094a55831d0fa23e6b4eebedb
nhy0052 <nhy0052@naver.com>
2022-07-11 02:02:27 +0900

    비밀번호 확인 팝업창

--- FrontEnd/js/mypage.js
+++ FrontEnd/js/mypage.js
@@ -1,4 +1,5 @@
 
+
 // 화면에 시각화 함수
 function showUser(exDatas) {
   // console.log(exDatas);
@@ -12,8 +13,6 @@ function showUser(exDatas) {
   const nextDate = document.querySelector("#next");
   nextDate.innerHTML = exDatas.result.nextDate.slice(0,10).replaceAll('-','.');
 }
-
-
 
 
 // 마이페이지 보여주기 
@@ -30,3 +29,36 @@ fetch('http://localhost:3000/api/privacy/user')
 
 
     });
+
+
+
+// 수정 페이지로 이동(비밀번호 확인)
+
+// 팝업 띄우기
+function showPopup() {
+	const popup = document.querySelector('#popup');
+  popup.classList.remove('hide');
+}
+
+// 팝업 닫기
+function closePopup(num) {
+	const popup = document.querySelector('#popup');
+  popup.classList.add('hide');
+
+
+  if(num){
+    // console.log("저장");
+    const inputPW = document.querySelector("#inputPW").value;
+    console.log(inputPW);
+
+    // inputPW를 서버로 보내서 일치하는지 확인하고 일치하면 editProfile.html로 로드, 불일치하면 아래에 비밀번호가 틀립니다! 띄우기
+
+
+  }
+  else{
+    console.log("취소");
+  }
+
+
+
+}
