29c36b6567065413a924be4666a73729721b3c73
belljun3395 <195850@jnu.ac.kr>
2022-07-11 21:40:59 +0900

    router 추가

--- BackEnd/src/routes/index.js
+++ BackEnd/src/routes/index.js
@@ -3,18 +3,30 @@ const router = express.Router();
 
 const passport = require('../middlewares/passport');
 
-
-router.get('/', passport.isLoggedIn, function(req, res, next) {
-  res.render('home');
+router.get('/after_register', passport.isLoggedIn, function(req, res, next) {
+  res.render('after_register');
 });
 
 router.get('/before_login', function(req, res, next) {
   res.render('before_login');
 });
 
-router.get('/after_register', passport.isLoggedIn, function(req, res, next) {
-  res.render('after_register');
+router.get("/designer", passport.isLoggedIn, function(req, res, next) {
+  res.render('designer');
 });
+
+router.get("/editProfile", passport.isLoggedIn, function(req, res, next) {
+  res.render('editProfile');
+});
+
+router.get('/', passport.isLoggedIn, function(req, res, next) {
+  res.render('home');
+});
+
+router.get("/mypage", passport.isLoggedIn, function(req, res, next) {
+  res.render('mypage');
+});
+
 
 router.get('/record', passport.isLoggedIn, function(req, res, next) {
   res.render('record');
@@ -28,7 +40,4 @@ router.get('/register_2', passport.isLoggedIn, function(req, res, next) {
   res.render('register_2');
 });
 
-router.get("/designer", passport.isLoggedIn, function(req, res, next) {
-  res.render('designer');
-})
 module.exports = router;
